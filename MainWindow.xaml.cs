0ef24baf3a1d36220da90524e37b8fd319b3a041
YDM <krad@hanmir.com>
2019-10-20 16:27:31 +0900

    ADD UI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,20 +1,9 @@
-﻿#define __DEBUG__
+﻿// #define __DEBUG__
 // https://www.tenforums.com/general-support/62662-how-clear-applications-get-notifications-these-senders.html
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 using System.Data.SQLite;
 
@@ -25,8 +14,8 @@ namespace GroupNotificationRemoverForWindows
     /// </summary>
     public partial class MainWindow : Window
     {
-        // private string DatabaseURL = "%USERPROFILE%\\AppData\\Local\\Microsoft\\Windows\\Notifications\\wpndatabase.db";
-        private SQLiteConnection conn;
+        private string DatabasePath = "Microsoft\\Windows\\Notifications\\wpndatabase.db"; // "%USERPROFILE%\\AppData\\Local\\Microsoft\\Windows\\Notifications\\wpndatabase.db";
+        private List<string> groupList = new List<string>();
 
         public MainWindow()
         {
@@ -36,29 +25,114 @@ namespace GroupNotificationRemoverForWindows
             test();
 #endif
             InitializeComponent();
+
+            Initialize();
+            Update();
+
+            removeButton.Click += RemoveProcess;
+        }
+
+        private void RemoveProcess(object sender, RoutedEventArgs args)
+        {
+            var items = GroupListView.SelectedItems;
+            var itemId = new List<int>();
+
+            try
+            {
+                var DatabaseURL = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), this.DatabasePath);
+                var conn = new SQLiteConnection("Data Source="+DatabaseURL);
+                conn.Open();
+
+                var command = new SQLiteCommand("select * from HandlerAssets", conn);
+                var result = command.ExecuteReader();
+                var schema = result.GetValues();
+
+                while(result.Read()) {
+                    foreach(string v in schema)
+                        if(v == "AssetKey" && result[v].ToString() == "DisplayName")
+                        {
+                            var assetValue = result["AssetValue"];
+                            if(items.Contains(assetValue))
+                                itemId.Add(int.Parse(result["HandlerId"].ToString()));
+                        }
+                }
+
+                foreach(var id in itemId)
+                {
+                    command = new SQLiteCommand($"delete from HandlerSettings where HandlerID = {id}", conn);
+                    command.ExecuteNonQuery();
+                    command = new SQLiteCommand($"delete from Notification where HandlerID = {id}", conn);
+                    command.ExecuteNonQuery();
+                    command = new SQLiteCommand($"delete from WNSPushChannel where HandlerID = {id}", conn);
+                    command.ExecuteNonQuery();
+                    // command = new SQLiteCommand($"delete from NotificationHandler where HandlerID = {id}", conn); // no HandlerID
+                    // command.ExecuteNonQuery();
+                    command = new SQLiteCommand($"delete from HandlerAssets where HandlerID = {id}", conn);
+                    command.ExecuteNonQuery();
+                }
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            foreach(string item in items)
+                groupList.Remove(item);
+            GroupListView.Items.Refresh();
+        }
+
+        private void Update()
+        {
+            var DatabaseURL = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), this.DatabasePath);
+            var conn = new SQLiteConnection("Data Source="+DatabaseURL);
+
+            try
+            {
+                conn.Open();
+
+                var command = new SQLiteCommand("select * from HandlerAssets", conn);
+                var result = command.ExecuteReader();
+                var schema = result.GetValues();
+
+                while(result.Read()) {
+                    foreach(string v in schema)
+                        if(v == "AssetKey" && result[v].ToString() == "DisplayName")
+                            groupList.Add(result["AssetValue"].ToString());
+                }
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            GroupListView.Items.Refresh();
+        }
+
+        private void Initialize()
+        {
+            this.GroupListView.ItemsSource = groupList;
         }
 
         private void test()
         {
             // get database path
-            var DatabaseURL = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), 
-                                            "Microsoft\\Windows\\Notifications\\wpndatabase.db");
+            var DatabaseURL = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DatabasePath);
             Console.WriteLine(DatabaseURL);
 
             // connect
-            this.conn = new SQLiteConnection("Data Source="+DatabaseURL);
+            var conn = new SQLiteConnection("Data Source="+DatabaseURL);
             try
             {
                 conn.Open();
 
                 // query
-                // var command = new SQLiteCommand("select * from NotificationHandler", this.conn);
-                var command = new SQLiteCommand("select * from HandlerAssets", this.conn);
+                var command = new SQLiteCommand("select * from HandlerAssets", conn);
                 var result = command.ExecuteReader();
                 var schema = result.GetValues();
                 foreach(var v in schema)
                 {
-                    // Console.WriteLine(v);
                     Console.Write(v+"\t");
                 }
                 Console.Write("\n");
@@ -77,7 +151,6 @@ namespace GroupNotificationRemoverForWindows
 
                     }
                     Console.WriteLine("");
-                    // Console.WriteLine(result);
                 }
                 result.Close();
 #endregion
@@ -97,7 +170,6 @@ namespace GroupNotificationRemoverForWindows
                     // 9. Enter and execute entry: "delete from WNSPushChannel where HandlerID = X".
                     // 10. Enter and execute entry: "delete from NotificationHandler where HandlerID = X".
                 }
-                // 11. Save file: "wpndatabase.db".
 #endregion
 
                 // disconnect
